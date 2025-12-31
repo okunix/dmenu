6e70769c0a1c73306ca74ce4256035894128f021
yosa12978 <yosa12978@gmail.com>
2025-12-31 18:37:01 +0300

    eyecandy update

--- config.h
+++ config.h
@@ -9,8 +9,8 @@ static const char *fonts[] = {
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
-	[SchemeNorm] = { "#c0caf5", "#1a1b26" },
-	[SchemeSel] = { "#ffffff", "#6495ed" },
+	[SchemeNorm] = { "#eeeeee", "#111111" },
+	[SchemeSel] = { "#111111", "#eeeeee" },
 	[SchemeOut] = { "#000000", "#00ffff" },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
