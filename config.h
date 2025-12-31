2672791cf16506092875f0c0709dcf3c40e38e03
yosa12978 <yosa12978@gmail.com>
2025-12-31 17:57:50 +0300

    updated colorscheme

--- config.h
+++ config.h
@@ -10,7 +10,7 @@ static const char *prompt      = NULL;      /* -p  option; prompt to the left of
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
 	[SchemeNorm] = { "#c0caf5", "#1a1b26" },
-	[SchemeSel] = { "#ffffff", "#6699cc" },
+	[SchemeSel] = { "#ffffff", "#6495ed" },
 	[SchemeOut] = { "#000000", "#00ffff" },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
