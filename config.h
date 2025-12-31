60e605fe22e6a4d0ea3d05d386778e1ddaffadff
yosa12978 <yosa12978@gmail.com>
2025-12-31 20:16:31 +0300

    color fix

--- config.h
+++ config.h
@@ -9,8 +9,8 @@ static const char *fonts[] = {
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
-	[SchemeNorm] = { "#c0caf5", "#1a1b26" },
-	[SchemeSel] = { "#1a1b26", "#c0caf5" },
+	[SchemeNorm] = { "#c0caf5", "#15161e" },
+	[SchemeSel] = { "#15161e", "#c0caf5" },
 	[SchemeOut] = { "#000000", "#00ffff" },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
